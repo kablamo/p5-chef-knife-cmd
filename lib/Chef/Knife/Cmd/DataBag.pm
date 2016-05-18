9be2209e2bd4dd4ab352ed921a8ddbc9c92846e2
Eric Johnson <eric.git@iijo.org>
2016-05-17 21:56:22 -0400

    Added $item param to $knife->data_bag->show()

--- lib/Chef/Knife/Cmd/DataBag.pm
+++ lib/Chef/Knife/Cmd/DataBag.pm
@@ -6,9 +6,11 @@ use Chef::Knife::Cmd::Node::RunList;
 has knife => (is => 'ro', required => 1, handles => [qw/handle_options run/]);
 
 sub show {
-    my ($self, $data_bag, %options) = @_;
+    my ($self, $data_bag, $item, %options) = @_;
     my @opts = $self->handle_options(%options);
-    my @cmd  = (qw/knife data bag show/, $data_bag, @opts);
+    my @cmd  = (qw/knife data bag show/, $data_bag);
+    push @cmd, $item if $item;
+    push @cmd, @opts;
     $self->run(@cmd);
 }
 
